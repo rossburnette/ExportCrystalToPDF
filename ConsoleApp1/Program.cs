37d3e1f0f08a047d864642e6f2882d33aa6d1fad
Ross Burnette <Ross Burnette>
2019-12-25 19:19:56 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReportDocument cryRpt = new ReportDocument();
+            cryRpt.Load(@"E:\Documents\visual studio 2017\Projects\ConsoleApp1\ConsoleApp1\Customers.rpt");
+            cryRpt.ExportToDisk(ExportFormatType.PortableDocFormat, @"E:\Documents\visual studio 2017\Projects\ConsoleApp1\ConsoleApp1\ASD.pdf");
+            MessageBox.Show("Exported Successful");
+        }
+    }
+}
